f755452c212a09f4b07a2598285c663d0bdcfbc6
wassen <kazuki-a@outlook.com>
2016-06-27 03:55:22 +0900

    check feature

--- src/makeGraph.py
+++ src/makeGraph.py
@@ -8,8 +8,8 @@ from lane_changing import Container, DataInput
 
 args = sys.argv
 # ここのやつ
-if len(args) != 3:
-    raise Exception("引数２つにしてね。\n{}が使えます。".format(','.join([item.value for item in list(Container.Features)])))
+if len(args) != 3 or not all(arg in [item.value for item in list(Features)] for arg in args[1:3]):
+    raise Exception("引数２つにして、\n{}を指定してください。".format(','.join([item.value for item in list(Features)])))
 feature1 = args[1]
 feature2 = args[2]
 
