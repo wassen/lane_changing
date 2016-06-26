413c25ba928e1a0bee36910d9460af34496ef89f
wassen <kazuki-a@outlook.com>
2016-06-27 03:26:04 +0900

    refine code

--- src/makeGraph.py
+++ src/makeGraph.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 import os
 import sys
-# ./makeGraph ttcx ttcy 使える引数を自動でエラーに出力したい。
-path = os.path.join(os.path.dirname(__file__), '../')
-sys.path.append(path)
-from lane_changing import Container, DataInput
+# ./makeGraph ttcx ttcy
+from main import Container, DataInput, Features
 
 args = sys.argv
 # ここのやつ
-if len(args) != 3:
-    raise Exception("引数２つにしてね。\n{}が使えます。".format(','.join([item.value for item in list(Container.Features)])))
+if len(args) != 3 or not all(arg in [item.value for item in list(Features)] for arg in args[1:3]):
+    raise Exception("引数２つにしてね。\n{}が使えます。".format(','.join([item.value for item in list(Features)])))
 feature1 = args[1]
 feature2 = args[2]
 
 #わかりやすく整理したい
 # loadの自動化
-ctn = Container(DataInput.readFromCSVData)
-# ctn.show_plot(Container.Features(feature1), Container.Features(feature2), load=True)
+ctn = Container(DataInput.loadOriginalData)
+ctn.show_plot(Features(feature1), Features(feature2), load=True)
 
