6118806813396911bd42542506965fee7cebf53d
wassen <kazuki-a@outlook.com>
2016-12-04 22:24:08 +0900

    csv output

--- src2/_tes.py
+++ src2/_tes.py
@@ -5,16 +5,25 @@ from os import listdir as ls
 from os.path import join
 
 import repo_env
-import driving_data as dd
+#import driving_data as dd
 
 import numpy as np
 import os
-np.save(os.path.join(repo_env.TMP_DIR, 'lc'),
-        [dd.behavior_and_drivingdata[behavior]['roa']['lc'] for behavior in dd.behavior_list])
-np.save(os.path.join(repo_env.TMP_DIR, 'blist'), dd.behavior_list)
+# np.save(os.path.join(repo_env.TMP_DIR, 'lc'),
+#        [dd.behavior_and_drivingdata[behavior]['roa']['lc'] for behavior in dd.behavior_list])
+# np.save(os.path.join(repo_env.TMP_DIR, 'blist'), dd.behavior_list)
 a = np.load(os.path.join(repo_env.TMP_DIR, 'lc.npy'))
 blist = np.load(os.path.join(repo_env.TMP_DIR, 'blist.npy'))
 import ddTools
 
 for c, b in zip(a, blist):
     print(b, len(ddTools.start_index(c)['right']), len(ddTools.start_index(c)['left']))
+
+import csv
+
+with open('some.csv', 'w') as f:
+    writer = csv.writer(f, lineterminator='\n')
+    for c, b in zip(a, blist):
+        print(b, len(ddTools.start_index(c)['right']), len(ddTools.start_index(c)['left']))
+        l = [b, len(ddTools.start_index(c)['right']), len(ddTools.start_index(c)['left'])] 
+        writer.writerow(l)
