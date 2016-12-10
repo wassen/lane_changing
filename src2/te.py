75a7f56491d244f73de099692b98537fc76ad413
wassen <kazuki-a@outlook.com>
2016-12-11 03:03:59 +0900

    pairplotっぽいもの2

--- src2/te.py
+++ src2/te.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 def pairplot(df, **kargs):
 
+    fig = plt.figure()
+
     if kargs.has_key("hue"):
         label = df[kargs["hue"]]
         # axisいんのか？
@@ -16,23 +18,47 @@ def pairplot(df, **kargs):
     feature_number = feature.shape[1]
     feature_names = feature.columns
 
-    comb = [(x,y) for x in feature_names for y in feature_names]
+    comb = [(x,y) for y in feature for x in feature]
 
-    def hist(feature):
+    lims = {fname: (min(feature[fname]), max(feature[fname])) for fname in feature}
 
-        lims = {fname:(min(feature[fname]), max(feature[fname])) for fname in feature}
+    # fig, subplots_2d = plt.subplots(nrows=feature_number, ncols=feature_number, sharex=True, sharey=True)
+    #
+    # from itertools import chain
+    # subplots = list(chain.from_iterable(subplots_2d))
 
-    for i, two_feature_names in enumerate(comb):
-        plt.subplot(feature_number, feature_number, i + 1)
-        if two_feature_names[0] == two_feature_names[1]:
-            plt.hist(feature[two_feature_names[0]])
-            plt.title('A tale of 2 subplots')
-            plt.ylabel('Damped oscillation')
+    sp_list=[]
+    scatter_flag=[False for _ in feature]
+    for i, fnames in enumerate(comb):
+        # sp_number = feature_number
+        # int(i/sp_number)
+        # i%sp_number
+
+        sp_row = list(feature_names).index(fnames[1])
+        sp_col = list(feature_names).index(fnames[0])
+
+        kwargs_sp = {}
+        if sp_row > 0:
+            kwargs_sp['sharex'] = sp_list[sp_col]
+        if sp_row != sp_col:
+            if scatter_flag[sp_row]:
+                kwargs_sp['sharey'] = sp_list[feature_number * sp_row + 0 if sp_row > 0 else 1]
+            else:
+                scatter_flag[sp_row] = True
+
+        sp = fig.add_subplot(feature_number, feature_number, i + 1, **kwargs_sp)
+        sp_list.append(sp)
+        # sp.set_xlim(lims[fnames[0]])
+        # sp.set_ylim(lims[fnames[1]])
+        if sp_row == sp_col:
+            sp.hist(feature[fnames[0]])
+            # sp.title = 'A tale of 2 subplots'
+            # sp.ylabel('Damped oscillation')
         else:
-            plt.scatter(*[feature[fname] for fname in two_feature_names])
-            plt.xlabel('time (s)')
-            plt.ylabel('Undamped')
-
+            sp.scatter(*[feature[fname] for fname in fnames])
+            # sp.xlabel('time (s)')
+            # sp.ylabel('Undamped')
+    return fig
 
 if __name__ == "__main__":
 
@@ -41,17 +67,13 @@ if __name__ == "__main__":
     import numpy as np
     import seaborn as sns
 
-    plt.hist([1,2,3])
+    # plt.hist([1,2,3])
 
-
-    plt.pairplot = pairplot
-
-    df = pd.DataFrame([[1, 2, 3, 'a'], [4, 5, 6, 'b'], [7, 8, 9, 'c']], columns=['a', 'b', 'c', 'label'])
-    plt.pairplot(df, hue='label')
-    plt.show()
+    df = pd.DataFrame([[1, 2, 3, 'f'], [6, 5, 4, 'g'], [7, 8, 9, 'h']], columns=['a', 'b', 'c', 'label'])
+    pairplot(df, hue='label').show()
 
     df = sns.load_dataset("iris")
     df = pd.DataFrame([[1, None, None, 'a'], [None, 3, 4, 'b'], [None, None, None, 'c']], columns=['a', 'b', 'c', 'label'])
-    df = pd.DataFrame([[1, 2, 3, 'a'], [4, 5, 6, 'b'], [7, 8, 9, 'c']], columns=['a', 'b', 'c', 'label'])
+    df = pd.DataFrame([[1, 2, 3, 'f'], [6, 5, 4, 'g'], [7, 8, 9, 'h']], columns=['a', 'b', 'c', 'label'])
     sns.pairplot(df, hue='label', dropna=True)
     sns.plt.show()
